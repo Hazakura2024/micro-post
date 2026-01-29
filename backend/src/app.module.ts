2245e7b473dbf77192ff596135019250d25663cb
yutaro <ytr240.jp@gmail.com>
2026-01-29 17:17:22 +0900

    feat: アプリケーションへの登録

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import AppDataSource from './app.datasource';
 
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [
+    // .env読み込み用
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // NOTE: アプリケーションへの登録
+    TypeOrmModule.forRoot(AppDataSource.options),
+    UserModule,
+    PostModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
