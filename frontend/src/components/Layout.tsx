e202685cab8df1be0f9abbac54dfffdfb1095e28
yutaro <ytr240.jp@gmail.com>
2026-02-09 15:50:15 +0900

    headerを配置

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import Header from './Header'
 
 const Layout = () => {
   return (
     <>
-
+        <SHeader>
+            <Header></Header>
+        </SHeader>
     </>
   )
 }
 
-
+const SHeader = styled.div`
+    width: 100%;
+    height: 32px;
+    border: 2ox solid red;
+`
 
 
 export default Layout
