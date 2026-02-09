0257d78b50db29ea021a2e625aa22202279a7827
yutaro <ytr240.jp@gmail.com>
2026-02-09 15:42:01 +0900

    fix: 不要なエクスポートを削除

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Post = () => {
+const Post = () => {
   return (
     <div>Post</div>
   )
--- frontend/src/components/PostList.tsx
+++ frontend/src/components/PostList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const PostList = () => {
+const PostList = () => {
   return (
     <div>PostList</div>
   )
