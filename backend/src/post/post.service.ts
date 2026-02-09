08be62d922140aed141f059c43ad2464d15fbacf
yutaro <ytr240.jp@gmail.com>
2026-02-09 12:51:21 +0900

    feat: createPostを作成

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Auth } from 'src/entities/auth';
+import { MicroPost } from 'src/entities/micropost';
+import { Equal, MoreThan, Repository } from 'typeorm';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(MicroPost)
+    private microPostRepository: Repository<MicroPost>,
+    @InjectRepository(Auth)
+    private AuthRepository: Repository<Auth>,
+  ) {}
+
+  async createPost(message: string, token: string) {
+    // ログイン済みかチェック
+    const now = new Date();
+    const auth = await this.AuthRepository.findOne({
+      where: {
+        token: Equal(token),
+        expire_at: MoreThan(now),
+      },
+    });
+    if (!auth) {
+      throw new ForbiddenException();
+    }
+
+    // レコードを作成
+    const record = {
+      user_id: auth.user_id,
+      content: message,
+    };
+    await this.microPostRepository.save(record);
+  }
+}
