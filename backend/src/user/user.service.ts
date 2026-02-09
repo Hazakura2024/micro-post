96173a0cfebde388a5995b0f6c6ca2d85075a094
yutaro <ytr240.jp@gmail.com>
2026-02-09 12:18:08 +0900

    feat: getUserを作成

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Auth } from 'src/entities/auth';
 import { User } from 'src/entities/user.entity';
-import { Repository } from 'typeorm';
+import { Equal, MoreThan, Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
@@ -12,4 +16,31 @@ export class UserService {
     @InjectRepository(Auth)
     private authRepository: Repository<Auth>,
   ) {}
+
+  //NOTE: ユーザー取得
+  async getUser(token: string, id: number) {
+    // ログイン済かチェック
+    const now = new Date();
+    const auth = await this.authRepository.findOne({
+      where: {
+        token: Equal(token),
+        expire_at: MoreThan(now),
+      },
+    });
+
+    if (!auth) {
+      throw new ForbiddenException();
+    }
+
+    // ユーザー取得
+    const user = await this.userRepository.findOne({
+      where: {
+        id: Equal(id),
+      },
+    });
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
 }
