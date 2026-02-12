2c0beca81f757b4cf524f3e4053edfa856cd1d5f
yutaro <ytr240.jp@gmail.com>
2026-02-12 10:44:30 +0900

    feat: axiosでgetListを記述

---
+++ frontend/src/api/Post.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import React from 'react'
+
+
+const Post = () => {
+  return 
+  (
+    <div>Post</div>
+  )
+}
+
+const getList = async() => {
+    const url =`http://localhost:3000/post?token={token}`;
+    const res = await axios.get(url)
+    return res;
+}
+
+export default Post
