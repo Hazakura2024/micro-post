e6256f1f1e280cee54202e0dabd61bb7248686e9
yutaro <ytr240.jp@gmail.com>
2026-02-12 10:57:18 +0900

    feat: signInコンポーネントを作成

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignIn = () => {
+  
+  const [userId, setUserId] = useState('');
+  const [pass, setPass] = useState('');
+
   return (
-    <div>SignIn</div>
+    <div>
+      <div>
+        <label htmlFor="id">ID</label>
+        <input id='id' value={userId} type="text" onChange={(evt) => setUserId(evt.target.value)}/>
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id='password' value={pass} type="text" onChange={(evt) => setPass(evt.target.value)}/>
+      </div>
+      <div>
+        <button type='button'>Login</button>
+      </div>
+    </div>
   )
 }
 
