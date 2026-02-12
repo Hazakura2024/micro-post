1c4ef55408a91f37d7ae6668039f300145b2883d
yutaro <ytr240.jp@gmail.com>
2026-02-12 12:22:09 +0900

    feat: post投稿UI作成

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SideBar = () => {
+
+  const [msg, setMsg] = useState('')
   return (
-    <div>SideBar</div>
+    <div>
+      <div>hoge</div>
+      <div>hoge@example.com</div>
+      <div>
+        <textarea rows ={4} value={msg} onChange={(e) => setMsg(e.target.value)}></textarea>
+      </div>
+      <div>
+        <button>送信</button>
+      </div>
+    </div>
   )
 }
 
