76bc45afe07e7e35167eb5fafb5850ab24dc25cc
yutaro <ytr240.jp@gmail.com>
2026-02-13 11:58:45 +0900

    feat: 非ログイン時に、ログインページに自動遷移

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../components/Layout'
 import { PostListProvider } from '../probviders/PostListProvider'
+import { UserContext } from '../probviders/UserProvider'
+import { Navigate } from 'react-router-dom'
 
 const Main = () => {
+  const { userInfo } = useContext(UserContext);
+  const loggedIn = (userInfo.token !== '');
+
   return (
     <PostListProvider>
-        <Layout></Layout>
+        {
+          loggedIn ? <Layout></Layout>:<Navigate replace to="/" />
+        }
     </PostListProvider>
   )
 }
