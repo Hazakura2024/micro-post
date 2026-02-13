282f9490af5e6a51fa2ea5ada218933573713453
yutaro <ytr240.jp@gmail.com>
2026-02-13 15:50:51 +0900

    refactor: APIのリンクを.envに格納

--- frontend/src/api/User.tsx
+++ frontend/src/api/User.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const getUser = async (id:number, token:string) => {
-    const url = `http://localhost:3000/user/${String(id)}?token=${token}`;
+export const getUser = async (id: number, token: string) => {
+    const API_URL = process.env.REACT_APP_API_URL;
+    const url = `${API_URL}/user/${String(id)}?token=${token}`;
     const res = await axios.get(url);
     console.log(res);
     return res.data;
