a22cdc90966215252ec8e960391630d61be4c7ac
yutaro <ytr240.jp@gmail.com>
2026-02-17 10:51:46 +0900

    refactor: axiosにレスポンスの型を指定

--- frontend/src/api/Post.tsx
+++ frontend/src/api/Post.tsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
+import { PostType } from '../types/Post';
 
 export const createPost = async (token: string, msg: string) => {
     const API_URL = process.env.REACT_APP_API_URL;
     const url = `${API_URL}/post?token=${token}`;
-    const res = await axios.post(
+    // (学習メモ): ここのvoidはレスポンスボディの型を指定しているだけで、resオブジェクト全体の型ではない。
+    const res = await axios.post<void>(
         url,
         {
             message: msg,
@@ -15,7 +17,7 @@ export const createPost = async (token: string, msg: string) => {
 export const getList = async (token: string) => {
     const API_URL = process.env.REACT_APP_API_URL;
     const url = `${API_URL}/post?records=20&token=${token}`;
-    const res = await axios.get(url)
+    const res = await axios.get<PostType[]>(url)
     return res.data;
 }
 
