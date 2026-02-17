b46db8d15dbde7035b7c8b166ce7e429ca009b47
yutaro <ytr240.jp@gmail.com>
2026-02-17 11:30:22 +0900

    feat: ログイン失敗時にエラーメッセージを表示

--- frontend/src/api/Auth.tsx
+++ frontend/src/api/Auth.tsx
@@ -2,10 +2,14 @@ import axios from 'axios';
 import { AuthResponse } from '../types/Auth';
 
 const signIn = async (userId: string, pass: string): Promise<AuthResponse> => {
-  const API_URL = process.env.REACT_APP_API_URL;
-  const url = `${API_URL}/auth?user_id=${userId}&password=${pass}`;
-  const res = await axios.get<AuthResponse>(url);
-  return res.data;
+  try {
+    const API_URL = process.env.REACT_APP_API_URL;
+    const url = `${API_URL}/auth?user_id=${userId}&password=${pass}`;
+    const res = await axios.get<AuthResponse>(url);
+    return res.data;
+  } catch (error : unknown) {
+    throw new Error(`認証失敗: ${error}`);
+  }
 }
 
 export default signIn;
