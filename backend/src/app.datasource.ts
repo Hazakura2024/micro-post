cebbc54ca6df60e9878003ca3f78936308ad0e52
yutaro <ytr240.jp@gmail.com>
2026-02-18 14:09:52 +0900

    refactor: db-portをenvで設定

--- backend/src/app.datasource.ts
+++ backend/src/app.datasource.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const AppDataSource = new DataSource({
   type: 'postgres',
   // NOTE:  資料にはないが追加
-  port: 5432,
+  port: parseInt(process.env.DB_PORT || '5432'),
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
