3d119abbf0b8c5ca5283fa91c6c28bcee4681df7
yutaro <ytr240.jp@gmail.com>
2026-02-18 13:10:04 +0900

    refactor: signInでtoken が undefined の場合の処理を記述

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -4,7 +4,8 @@ import styled from "styled-components";
 
 // (学習メモ): propsはオブジェクトになるので、左側に分割代入
 const Post = ({ post }: { post: PostType }) => {
-  const dateString = `${post.created_at}Z`
+  // NOTE: サーバーがZをつけて、UTCであるという指定をしていないので、ここでつける 
+  const dateString = `${post.created_at}Z`;
   const date = new Date(dateString);
   const getDateString = (dateObj: Date) => {
     return dateObj.toLocaleString();
