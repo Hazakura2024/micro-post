db5b89fd40bc2867afc1790033021db7dce846f0
yutaro <ytr240.jp@gmail.com>
2026-02-18 12:03:51 +0900

    refactor: ユーザー名表示のエラーハンドリングの修正漏れを修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -28,11 +28,7 @@ const Header = () => {
   };
 
   useEffect(() => {
-    try {
-      getMyUser();
-    } catch (error: unknown) {
-      console.log("ユーザー情報が取得できませんでした。");
-    }
+    getMyUser();
   }, []);
 
   return (
