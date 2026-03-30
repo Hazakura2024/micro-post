c266c8bf639345de84fd701a65e015a09a8c40f2
yutaro <ytr240.jp@gmail.com>
2026-03-30 14:31:58 +0900

    fix: apiフェッチコンポーネントの不要なtry-catchを削除

--- frontend/src/api/User.tsx
+++ frontend/src/api/User.tsx
@@ -5,15 +5,12 @@ export const getUser = async (
   id: number,
   token: string,
 ): Promise<UserResponse> => {
-  try {
-    const API_URL = import.meta.env.VITE_API_URL;
-    const url = `${API_URL}/user/${String(id)}?token=${token}`;
-    const res = await axios.get<UserResponse>(url);
-    return res.data;
-  } catch (error: unknown) {
-    // NOTE: エラーは自動的にthrowされコンポーネント側でextractErrorMessageを使う
-    throw error;
-  }
+  const API_URL = import.meta.env.VITE_API_URL;
+  const url = `${API_URL}/user/${String(id)}?token=${token}`;
+  const res = await axios.get<UserResponse>(url);
+  return res.data;
+  // NOTE: エラーは自動的にthrowされコンポーネント側でextractErrorMessageを使う
+
 };
 
 export const createUser = async (
@@ -21,30 +18,25 @@ export const createUser = async (
   email: string,
   password: string,
 ): Promise<UserResponse> => {
-  try {
-    const API_URL = import.meta.env.VITE_API_URL;
-    const url = `${API_URL}/user`;
-    const res = await axios.post<UserResponse>(url, {
-      name: name,
-      email: email,
-      password: password,
-    });
-    return res.data;
-  } catch (error: unknown) {
-    // NOTE: エラーは自動的にthrowされコンポーネント側でextractErrorMessageを使う
-    throw error;
-  }
+
+  const API_URL = import.meta.env.VITE_API_URL;
+  const url = `${API_URL}/user`;
+  const res = await axios.post<UserResponse>(url, {
+    name: name,
+    email: email,
+    password: password,
+  });
+  return res.data;
+
 };
 
 export const editUser = async (token: string, name: string) => {
-  try {
-    const API_URL = import.meta.env.VITE_API_URL;
-    const url = `${API_URL}/user/me?token=${token}`;
-    const res = await axios.patch(url, {
-      name: name,
-    });
-    return res.data;
-  } catch (error) {
-    throw error;
-  }
+
+  const API_URL = import.meta.env.VITE_API_URL;
+  const url = `${API_URL}/user/me?token=${token}`;
+  const res = await axios.patch(url, {
+    name: name,
+  });
+  return res.data;
+
 }
