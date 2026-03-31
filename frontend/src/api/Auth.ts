5c1fe2b561960be6abecd3815d37335a39147f25
yutaro <ytr240.jp@gmail.com>
2026-03-31 15:58:39 +0900

    fix: Login:getAuthをgetからpostに変更し、bodyで情報を送る

--- frontend/src/api/Auth.ts
+++ frontend/src/api/Auth.ts
@@ -4,8 +4,11 @@ import type { AuthResponse } from "../types/Auth";
 const signIn = async (userId: string, pass: string): Promise<AuthResponse> => {
 
   const API_URL = import.meta.env.VITE_API_URL;
-  const url = `${API_URL}/auth?user_id=${userId}&password=${pass}`;
-  const res = await axios.get<AuthResponse>(url);
+  const url = `${API_URL}/auth`;
+  const res = await axios.post<AuthResponse>(url, {
+    user_id: userId,
+    password: pass,
+  });
   return res.data;
 
 };
