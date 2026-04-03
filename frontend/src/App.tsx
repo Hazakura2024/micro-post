10631c70553e71f1121f12839199726998a4a7eb
yutaro <ytr240.jp@gmail.com>
2026-04-03 13:47:41 +0900

    feat: interceptorsカスタムフックをapp.tsxで呼び出し

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,25 @@ import SignIn from "./pages/SignIn";
 import Main from "./pages/Main";
 import { UserProvider } from "./providers/UserProvider";
 import { ToastContainer } from "react-toastify";
+import { useAxiosIntercepter } from "./hooks/useAxiosInterCepter";
+
+const AxiosIntercepterSetup = ({ children }: { children: React.ReactNode }) => {
+  // (学習メモ):  ここでフックを呼び出すことで、アプリ全体にインターセプターが適用される
+  useAxiosIntercepter();
+  return <>{children}</>
+}
 
 function App() {
   return (
     <div className="App">
       <ToastContainer position="top-right" />
       <UserProvider>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/main" element={<Main />} />
-        </Routes>
+        <AxiosIntercepterSetup>
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/main" element={<Main />} />
+          </Routes>
+        </AxiosIntercepterSetup>
       </UserProvider>
     </div>
   );
