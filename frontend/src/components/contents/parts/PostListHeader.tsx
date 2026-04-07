7378e80d4c98da681c19ba43ad0eb5eb0162e45e
yutaro <ytr240.jp@gmail.com>
2026-04-07 13:59:45 +0900

    refactor: postlistの分離のcss修正

--- frontend/src/components/contents/parts/PostListHeader.tsx
+++ frontend/src/components/contents/parts/PostListHeader.tsx
@@ -4,29 +4,29 @@ import { usePostList } from "../hooks/usePostList";
 
 export const PostListHeader = () => {
 
-    const {
-        isLoading,
-        onClickReload,
-        localSearchWord,
-        onChangeSearchWord,
-        localSearchName,
-        onChangeSearchName,
-        onClickSearch,
-        onClickClear,
-    } = usePostList()
+  const {
+    isLoading,
+    onClickReload,
+    localSearchWord,
+    onChangeSearchWord,
+    localSearchName,
+    onChangeSearchName,
+    onClickSearch,
+    onClickClear,
+  } = usePostList()
 
-    return (
-        <SHeader>
-            <div>投稿一覧 </div>
-            <SReloadButton disabled={isLoading} onClick={onClickReload}>
-                更新
-            </SReloadButton>
-            <input type="text" value={localSearchWord} onChange={(e) => onChangeSearchWord(e)} placeholder="内容を検索" />
-            <input type="text" value={localSearchName} onChange={(e) => onChangeSearchName(e)} placeholder="ユーザーの投稿を検索" />
-            <SMiniButton onClick={onClickSearch}>検索</SMiniButton>
-            <SMiniButton onClick={onClickClear}>クリア</SMiniButton>
-        </SHeader>
-    )
+  return (
+    <SHeader>
+      <div>投稿一覧 </div>
+      <SReloadButton disabled={isLoading} onClick={onClickReload}>
+        更新
+      </SReloadButton>
+      <SInputSearch type="text" value={localSearchWord} onChange={(e) => onChangeSearchWord(e)} placeholder="内容を検索" />
+      <SInputSearch type="text" value={localSearchName} onChange={(e) => onChangeSearchName(e)} placeholder="ユーザーの投稿を検索" />
+      <SMiniButton onClick={onClickSearch}>検索</SMiniButton>
+      <SMiniButton onClick={onClickClear}>クリア</SMiniButton>
+    </SHeader>
+  )
 }
 
 export default PostListHeader
@@ -41,11 +41,16 @@ const SHeader = styled.div`
   padding-left: 8px;
 `;
 
+const SInputSearch = styled.input`
+  width: 150px;
+  margin: 0px 2px;
+`
+
 const SReloadButton = styled.button`
   background-color: #00a3af;
   border-color: #eeeeee;
   padding: 4px;
-  margin: 16px;
+  margin: 10px;
   border-radius: 8px;
   color: #fafafa;
   width: 96px;
@@ -59,7 +64,7 @@ const SMiniButton = styled.button`
   background-color: #00a3af;
   border-color: #eeeeee;
   padding: 4px;
-  margin: 6px;
+  margin: 0px 3px;
   border-radius: 8px;
   color: #fafafa;
   width: 54px;
